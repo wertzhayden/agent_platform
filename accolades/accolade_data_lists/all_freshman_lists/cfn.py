32850f71e9254d7b011d91dd639de4e848051c5d
Hayden <wertzhayden@gmail.com>
2025-06-28 09:13:15 -0400

    adding accolades lists

---
+++ accolades/accolade_data_lists/all_freshman_lists/cfn.py
@@ -0,0 +1,185 @@
+"""Default 2024 CFN Freshman All-American List."""
+
+
+{
+  "cfn": {
+    "Offense": [
+      {"position":"QB","name":"DJ Lagway","school":"Florida Gators"},
+      {"position":"RB","name":"Fluff Bothwell","school":"South Alabama Jaguars"},
+      {"position":"RB","name":"Ahmad Hardy","school":"Louisiana-Monroe Warhawks"},
+      {"position":"WR","name":"Jeremiah Smith","school":"Ohio State Buckeyes"},
+      {"position":"WR","name":"Cam Coleman","school":"Auburn Tigers"},
+      {"position":"WR","name":"Ryan Williams","school":"Alabama Crimson Tide"},
+      {"position":"TE","name":"Decker DeGraaf","school":"Washington Huskies"},
+      {"position":"AP","name":"Isaac Brown","school":"Louisville Cardinals"},
+      {"position":"OT","name":"Jordan Seaton","school":"Colorado Buffaloes"},
+      {"position":"G","name":"Amare Grayson","school":"Jacksonville State Gamecocks"},
+      {"position":"C","name":"Tyler Mercer","school":"North Texas Mean Green"},
+      {"position":"G","name":"Aidan Banfield","school":"North Carolina Tar Heels"},
+      {"position":"OT","name":"Anthonie Knapp","school":"notre-dame"},
+    ],
+    "Offense_HonorableMention": [
+      {"position":"QB","name":"CJ Bailey","school":"NC State Wolfpack"},
+      {"position":"RB","name":"Wayshawn Parker","school":"Washington State Cougars"},
+      {"position":"RB","name":"Caden Durham","school":"LSU Tigers"},
+      {"position":"WR","name":"Emmett Mosley V","school":"Stanford Cardinal"},
+      {"position":"WR","name":"Nick Marsh","school":"Michigan State Spartans"},
+      {"position":"WR","name":"Joseph Williams","school":"Tulsa Golden Hurricane"},
+      {"position":"TE","name":"Landen Thomas","school":"Florida State Seminoles"},
+      {"position":"AP","name":"Jacorey Barner Jr.","school":"Nebraska Cornhuskers"},
+      {"position":"OT","name":"Josiah Thompson","school":"South Carolina Gamecocks"},
+      {"position":"G","name":"TJ Shanahan","school":"Texas A&M Aggies"},
+      {"position":"C","name":"Jacob Norcross","school":"Kennesaw State Owls"},
+      {"position":"G","name":"Braydon Nelson","school":"North Texas Mean Green"},
+      {"position":"OT","name":"Zach Clayton","school":"Middle Tennessee State Blue Raiders"}
+    ],
+    "Defense": [
+      {"position":"EDGE","name":"Dylan Stewart","school":"South Carolina Gamecocks"},
+      {"position":"EDGE","name":"Colin Simmons","school":"Texas Longhorns"},
+      {"position":"DL","name":"Malachi Ta'ase","school":"New Mexico State Aggies"},
+      {"position":"DL","name":"Maraad Watson","school":"Syracuse Orange"},
+      {"position":"LB","name":"Sammy Brown","school":"Clemson Tigers"},
+      {"position":"LB","name":"Chris Jones","school":"Southern Miss Eagles"},
+      {"position":"LB","name":"Simeon Coleman","school":"Cincinnati Bearcats"},
+      {"position":"CB","name":"Leonard Moore","school":"Notre Dame Fighting Irish"},
+      {"position":"CB","name":"Ashton Hampton","school":"Clemson Tigers"},
+      {"position":"CB","name":"Kelvin Hill","school":"UAB Blazers"},
+      {"position":"S","name":"Koi Perich","school":"Minnesota Golden Gophers"},
+      {"position":"S","name":"Ja’Marley Riddle","school":"East Carolina Pirates"},
+      {"position":"FLEX","name":"CJ Heard","school":"Florida Atlantic Owls"}
+    ],
+    "Defense_HonorableMention": [
+      {"position":"EDGE","name":"Sincere Edwards","school":"Pittsburgh Panthers"},
+      {"position":"EDGE","name":"Kyran Duhon","school":"UTEP Miners"},
+      {"position":"DL","name":"Jayden Jackson","school":"Oklahoma Sooners"},
+      {"position":"DL","name":"Stephiylan Green","school":"Clemson Tigers"},
+      {"position":"LB","name":"Jayden Studio","school":"Kent State Golden Flashes"},
+      {"position":"LB","name":"Damaine Wilson","school":"Georgia State Panthers"},
+      {"position":"LB","name":"Khmori House","school":"Washington Huskies"},
+      {"position":"CB","name":"Zabien Brown","school":"Alabama Crimson Tide"},
+      {"position":"CB","name":"OJ Frederique Jr.","school":"Miami (FL) Hurricanes"},
+      {"position":"CB","name":"Cameron Calhoun","school":"Utah Utes"},
+      {"position":"S","name":"Kaleb Harris","school":"Auburn Tigers"},
+      {"position":"S","name":"KJ Bolden","school":"Georgia Bulldogs"},
+      {"position":"FLEX","name":"Eli Bowen","school":"Oklahoma Sooners"}
+    ],
+    "SpecialTeams": [
+      {"position":"K","name":"Caden Chittenden","school":"UNLV Rebels"},
+      {"position":"P","name":"Michael Salgado-Medina","school":"Arizona Wildcats"},
+      {"position":"KR","name":"Lloyd Avant","school":"Tulsa Golden Hurricane"},
+      {"position":"PR","name":"Boo Carter","school":"Tennessee Volunteers"},
+      {"position":"LS","name":"James Roe","school":"Toledo Rockets"}
+    ],
+    "SpecialTeams_HonorableMention": [
+      {"position":"K","name":"Nolan Hauser","school":"Clemson Tigers"},
+      {"position":"P","name":"Rhys Dakin","school":"Iowa Hawkeyes"},
+      {"position":"P","name":"Kade Reynoldson","school":"Duke Blue Devils"},
+      {"position":"KR","name":"Solomon Beebe","school":"UAB Blazers"},
+      {"position":"PR","name":"Koi Perich","school":"Minnesota Golden Gophers"},
+      {"position":"LS","name":"Hyrum Hatch","school":"New Mexico Lobos"}
+    ]
+  },
+  "Awards": {
+      "FreshmanPlayerOfTheYear": {
+        "name": "Koi Perich",
+        "position": "S",
+        "school": "Minnesota Golden Gophers"
+      },
+      "OffensivePlayerOfTheYear": {
+        "name": "Jeremiah Smith",
+        "position": "WR",
+        "school": "Ohio State Buckeyes"
+      },
+      "DefensivePlayerOfTheYear": {
+        "name": "Dylan Stewart",
+        "position": "EDGE",
+        "school": "South Carolina Gamecocks"
+      },
+      "SpecialistOfTheYear": {
+        "name": "Caden Chittenden",
+        "position": "K",
+        "school": "UNLV Rebels"
+      }
+    },
+    "OffensePositionAwards": {
+      "QuarterbackOfTheYear": {
+        "name": "DJ Lagway",
+        "position": "QB",
+        "school": "Florida Gators"
+      },
+      "RunningBackOfTheYear": {
+        "name": "Fluff Bothwell",
+        "position": "RB",
+        "school": "South Alabama Jaguars"
+      },
+      "WideReceiverOfTheYear": {
+        "name": "Jeremiah Smith",
+        "position": "WR",
+        "school": "Ohio State Buckeyes"
+      },
+      "TightEndOfTheYear": {
+        "name": "Decker DeGraaf",
+        "position": "TE",
+        "school": "Washington Huskies"
+      },
+      "OffensiveTackleOfTheYear": {
+        "name": "Jordan Seaton",
+        "position": "OT",
+        "school": "Colorado Buffaloes"
+      },
+      "OffensiveGuardOfTheYear": {
+        "name": "Amare Grayson",
+        "position": "G",
+        "school": "Jacksonville State Gamecocks"
+      },
+      "CenterOfTheYear": {
+        "name": "Tyler Mercer",
+        "position": "C",
+        "school": "North Texas Mean Green"
+      }
+    },
+    "DefensePositionAwards": {
+      "EdgeDefenderOfTheYear": {
+        "name": "Dylan Stewart",
+        "position": "EDGE",
+        "school": "South Carolina Gamecocks"
+      },
+      "InteriorDefenderOfTheYear": {
+        "name": "Malachi Ta’ase",
+        "position": "DL",
+        "school": "New Mexico State Aggies"
+      },
+      "LinebackerOfTheYear": {
+        "name": "Sammy Brown",
+        "position": "LB",
+        "school": "Clemson Tigers"
+      },
+      "CornerbackOfTheYear": {
+        "name": "Leonard Moore",
+        "position": "CB",
+        "school": "Notre Dame Fighting Irish"
+      },
+      "SafetyOfTheYear": {
+        "name": "Koi Perich",
+        "position": "S",
+        "school": "Minnesota Golden Gophers"
+      }
+    },
+    "SpecialTeamsPositionAwards": {
+      "KickerOfTheYear": {
+        "name": "Caden Chittenden",
+        "position": "K",
+        "school": "UNLV Rebels"
+      },
+      "PunterOfTheYear": {
+        "name": "Michael Salgado-Medina",
+        "position": "P",
+        "school": "Arizona Wildcats"
+      },
+      "ReturnerOfTheYear": {
+        "name": "Lloyd Avant",
+        "position": "KR",
+        "school": "Tulsa Golden Hurricane"
+      }
+    }
+}
